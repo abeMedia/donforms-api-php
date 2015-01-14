b20e5f75ba29a1efd39be1e1c08b9faf12490a04
koding user <adam@abemedia.co.uk>
2015-01-14 04:21:23 +0000

    Cleanup

--- src/donForms/Entries.php
+++ src/donForms/Entries.php
@@ -5,18 +5,18 @@ namespace donForms\donForms;
 class Entries extends \donForms\donForms
 {
     public function getList($form) {
-        return $this->client->get($this->api_endpoint . '/entries/' . $form);
+        return $this->client->get($this->api_endpoint . 'entries/' . $form);
     }
     
     public function get($id) {
-        return $this->client->get($this->api_endpoint . '/entry/' . $id);
+        return $this->client->get($this->api_endpoint . 'entry/' . $id);
     }
     
     public function update($id, $params) {
-        return $this->client->put($this->api_endpoint . '/entry/' . $id, $params);
+        return $this->client->put($this->api_endpoint . 'entry/' . $id, $params);
     }
     
     public function delete($id) {
-        return $this->client->delete($this->api_endpoint . '/entry/' . $id);
+        return $this->client->delete($this->api_endpoint . 'entry/' . $id);
     }
 }
