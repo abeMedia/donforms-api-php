c5bad96e6aeb5800c4b33c9bf52566772b0e39bc
koding user <adam@abemedia.co.uk>
2015-01-14 03:20:34 +0000

    Update API urls

---
+++ src/donForms/Entries.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace donForms\donForms;
+
+class Entries extends \donForms\donForms
+{
+    public function getList($form) {
+        return $this->client->get($this->api_endpoint . '/entries/' . $form);
+    }
+    
+    public function get($id) {
+        return $this->client->get($this->api_endpoint . '/entry/' . $id);
+    }
+    
+    public function update($id, $params) {
+        return $this->client->put($this->api_endpoint . '/entry/' . $id, $params);
+    }
+    
+    public function delete($id) {
+        return $this->client->delete($this->api_endpoint . '/entry/' . $id);
+    }
+}
